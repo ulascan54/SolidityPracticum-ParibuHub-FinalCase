95ffcf99b3f647fc9420ef9161575a7640f6f067
Ulaş Can <ulascan.demirbag@std.yeditepe.edu.tr>
2023-01-15 21:40:24 +0300

    refactoring the footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,21 +1,22 @@
 import MainLogo from '../assests/PARIBU-NFT-logos-transparent.png'
 
 const Footer = () => (
-    <div className='w-full flex flex-col md:justify-center justify-between items-center p-4 gradient-bg-footer'>
-        <div className='w-full flex sm:flex-row flex-col justify-between items-center my-4'>
-            <div className='flex flex-[0.45] justify-center'>
-                <img className=' w-60' src={MainLogo} alt="Main Logo" />
+    <div className='footer-container'>
+        <div>
+            <div className='footer-logo'>
+                <img src={MainLogo} alt="Main Logo" />
             </div>
-            <div className='flex flex-1 justify-evenly items-center flex-wrap sm:mt-0 mt-5 w-full text-white text-base text-center'>
-                <p className=' cursor-pointer mx-2'>Market</p>
-                <p className=' cursor-pointer mx-2'>Artists</p>
-                <p className=' cursor-pointer mx-2'>Features</p>
-                <p className=' cursor-pointer mx-2'>Community</p>
+            <div className='footer-menu'>
+                <p>Market</p>
+                <p>Artists</p>
+                <p>Features</p>
+                <p>Community</p>
             </div>
-            <div className='flex flex-[0.25] flex-col-reverse items-center'>
-                <p className='text-white text-right text-sm'>&coppy;2023 All rights revserved.</p>
+            <div className='footer-info'>
+                <p>&coppy;2023 All rights revserved.</p>
             </div>
         </div>
+        <p>Created By <a href="https://www.linkedin.com/in/ulascandemirbag/">Ulaş Can Demirbağ</a></p>
     </div>
 )
 
