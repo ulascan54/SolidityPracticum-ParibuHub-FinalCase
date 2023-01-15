05f3b5f9df746288b9cf77346be71e7250f60dea
Ulaş Can <ulascan.demirbag@std.yeditepe.edu.tr>
2023-01-15 21:24:14 +0300

    refactoring the artworks component

--- src/components/Artworks.jsx
+++ src/components/Artworks.jsx
@@ -6,10 +6,10 @@ const imgArts = "https://images.cointelegraph.com/images/1434_aHR0cHM6Ly9zMy5jb2
 
 const Artworks = () => {
 return (
-    <div className="bg-[#151c25] gradient-bg-artworks">
-        <div className="w-4/5 py-10 mx-auto">
-            <h4 className="text-white text-3xl font-bold uppercase text-gradient">Latest Artworks</h4>
-            <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-6 md:gap-4 lg:gap-3 py-2.5">
+    <div className="artworks-container gradient-bg-artworks">
+        <div>
+            <h4 className="text-gradient">Latest Artworks</h4>
+            <div className="artworks-items-container">
                 {Array(4)
                 .fill()
                 .map((nft,i)=>(
@@ -27,16 +27,16 @@ return (
 
 
 const Card = ({ nft }) => (
-    <div className="w-full shadow-xl shadow-black rounded-md overflow-hidden bg-gray-800 my-2 p-3">
-        <img className="h-60 w-full object-cover shadow-lg shadow-black rounded-lg mb-3" src={imgArts} alt={"NFT Image"} />
-        <h4 className="text-white font-semibold">NFT #{nft}</h4>
-        <p className=" text-gray-400 text-sm my-1">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Hic laudantium, unde, ipsam odio molestias, eligendi illo nisi error delectus ad ratione fuga. Voluptatibus dolorem, quia mollitia saepe neque earum atque laudantium assumenda sequi molestias amet totam vitae vel ratione tempora veniam itaque necessitatibus accusamus laboriosam adipisci consequatur tempore optio nemo!</p>
-        <div className="flex justify-between items-center text-white">
-            <div className=" flex flex-col">
-                <small className="text-xs">Current Price</small>
-                <p className="text-sm font-semibold">0.34 ETH</p>
+    <div className="artwork-container">
+        <img src={imgArts} alt={"NFT Image"} />
+        <h4>NFT #{nft}</h4>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Hic laudantium, unde, ipsam odio molestias, eligendi illo nisi error delectus ad ratione fuga. Voluptatibus dolorem, quia mollitia saepe neque earum atque laudantium assumenda sequi molestias amet totam vitae vel ratione tempora veniam itaque necessitatibus accusamus laboriosam adipisci consequatur tempore optio nemo!</p>
+        <div>
+            <div>
+                <small>Current Price</small>
+                <p>0.34 ETH</p>
             </div>
-            <button className='shadow-md shadow-black text-white bg-[#476ec2] hover:bg-[#3453b6] hover:underline md:text--xs px-1.5 py-1 rounded-full'
+            <button className='artwork-button'
             onClick={()=> {
                 setGlobalState('showModal','animate__bounceIn block');
                 setGlobalState('showModalBg','animate__fadeIn block animate__faster');
