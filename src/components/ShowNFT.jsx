13d4e51b6eeed4cc95b982f13ce47ea5ad2c3bed
Ulaş Can <ulascan.demirbag@std.yeditepe.edu.tr>
2023-01-15 22:33:05 +0300

    refactoring the shownft component

--- src/components/ShowNFT.jsx
+++ src/components/ShowNFT.jsx
@@ -22,47 +22,47 @@ const ShowNFT = () => {
     return (
         <div className={`popup-container flex animate__animated  ${showModalBg}`}>
             <div className={`animate__animated  ${showModal}`}>
-                <div className="flex flex-col">
-                    <div className="flex justify-between text-gray-400 items-center">
-                        <p className=" font-semibold ">Buy NFT</p>
-                        <button type="button" className="border-0 bg-transparent focus:outline-none" onClick={closeModal}>
+                <div className="shownft-container">
+                    <div>
+                        <p>Buy NFT</p>
+                        <button type="button" onClick={closeModal}>
                             <FaTimes />
                         </button>
                     </div>
 
-                    <div className="flex justify-center items-center rounded-xl mt-5 ">
-                        <div className="shrink-0 h-40 w-40 rounded-xl overflow-hidden">
-                            <img className="w-full object-cover h-full cursor-pointer" src={imgHero} alt="NFT" />
+                    <div>
+                        <div>
+                            <img src={imgHero} alt="NFT" />
                         </div>
                     </div>
 
-                    <div className='flex flex-col justify-start rounded-xl mt-5'>
-                        <h4 className='text-white font-semibold'>Title</h4>
-                        <p className='text-gray-400 text-xs my-1'>
+                    <div>
+                        <h4>Title</h4>
+                        <p>
                             Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum reprehenderit cumque, unde quibusdam, labore, iure similique quos corrupti soluta quam id molestias dolore nostrum ratione non expedita fugit. Eligendi, id.
                         </p>
-                        <div className='flex justify-between items-center mt-3 text-white'>
-                            <div className='flex justify-start items-center'>
-                                <Identicon className="h-10 object-contain rounded-full mr-3" string={'adefaefas'} size={50} />
-                                <div className='flex flex-col justify-center items-start'>
-                                    <small className='text-white font-bold'>@Owner</small>
-                                    <small className='text-blue-400 font-semibold'>0x3213123214.2312421...213</small>
+                        <div>
+                            <div>
+                                <Identicon className="identicon" string={'adefaefas'} size={50} />
+                                <div>
+                                    <small>@Owner</small>
+                                    <small>0x3213123214.2312421...213</small>
                                 </div>
                             </div>
 
-                            <div className='flex flex-col'>
-                                <small className='text-xs'>Current Price</small>
-                                <p className='text-sm font-semibold'>0.34 ETH</p>
+                            <div>
+                                <small>Current Price</small>
+                                <p>0.34 ETH</p>
                             </div>
                         </div>
                     </div>
 
-                    <div className='flex justify-between items-center space-x-2'>
-                        <button className="flex justify-center items-center shadow-lg shadow-black text-md   text-white bg-[#476ec2] hover:bg-[#3453b6] md:text--xs p-2 rounded-full mt-5 w-full">
+                    <div>
+                        <button>
                             Purchase
                         </button>
 
-                        <button className="flex justify-center items-center shadow-lg shadow-black text-md   text-white bg-[#476ec2] hover:bg-[#3453b6] md:text--xs p-2 rounded-full mt-5 w-full"
+                        <button
                             onClick={() => {
                                 setGlobalState('updateModal','animate__bounceIn block');
                                 setGlobalState('updateModalBg','animate__fadeIn block animate__faster');
