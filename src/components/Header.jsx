77d5bc99744e82d3b83f4b45ec4947a7e237c058
Ulaş Can <ulascan.demirbag@std.yeditepe.edu.tr>
2023-01-15 20:30:45 +0300

    refactoring header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,19 @@
 import MainLogo from '../assests/PARIBU-NFT-logos-transparent.png'
 const Header = () => {
   return (
-    <div className="w-4/5  flex md:justify-center justify-between mx-auto items-center py-4">
-        <div className='md:flex-[0.5] flex-initial justify-center items-center'>
-            <img className="w-40 cursor-pointer" src={MainLogo} alt="logo" />
+    <div className="header-container">
+        <div className='header-logo-container'>
+            <img className="header-logo" src={MainLogo} alt="logo" />
         </div>
 
-        <ul className='md:flex-[0.5] text-white md:flex  hidden list-none justify-between items-center flex-initial '>
-          <li className='mx-4 cursor-pointer'>Market</li>
-          <li className='mx-4 cursor-pointer'>Artist</li>
-          <li className='mx-4 cursor-pointer'>Features</li>
-          <li className='mx-4 cursor-pointer'>Community</li>
+        <ul className='header-menu-container'>
+          <li className='header-menu-item'>Market</li>
+          <li className='header-menu-item'>Artist</li>
+          <li className='header-menu-item'>Features</li>
+          <li className='header-menu-item'>Community</li>
         </ul>
 
-        <button className='shadow-xl shadow-black text-white bg-[#476ec2] hover:bg-[#3453b6] md:text--xs p-2 rounded-full'>
+        <button className='header-button'>
           Connect Wallet
         </button>
     </div>
